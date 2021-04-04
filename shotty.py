b3f77706a20ca4d2556c7324f91720e491604ee8
Mauro Boffito <mauro90.07@gmail.com>
2021-04-03 22:30:46 -0300

    Added start/stop function

--- shotty.py
+++ shotty.py
@@ -5,22 +5,77 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_ec2():
+#Definimos un grupo de commandos llamado Intances
+@click.group()
+def instances():
+    """Commands for Instances"""
+
+#Ahora la accion LIST es necesaria para mostrar por defecto las instances
+@instances.command('list', help="List all instances")
+@click.option('--project', default=None, help="Targets all instances belonging to a project")
+def list_ec2(project):
     "List EC2 Instances"
-    for i in ec2.instances.all():
+    
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+        
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        dns = {i.public_dns_name or "N/A"}
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            dns.pop(),
+            tags.get('Project','No Project'))))
     return
+
+@instances.command('stop', help="Stop all instances")
+@click.option('--project', default=None, help="Targets all instances belonging to a project")        
+def ec2_stop(project):
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
         
+    for i in instances:
+        if i.state['Name'] == "stopped":
+            print("Instances {0} is already stopped".format(i.id))
+        else:
+            resp=i.stop()
+            print("Stopping Instance: {0}...State: {1}".format(i.id,resp['StoppingInstances'][0]['CurrentState']['Name']))
+    return
+
+@instances.command('start', help="Start all instances")
+@click.option('--project', default=None, help="Targets all instances belonging to a project")        
+def ec2_stop(project):
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
         
+    for i in instances:
+        if i.state['Name'] == "started":
+            print("Instances {0} is already started".format(i.id))
+        else:
+            resp=i.start()
+            print("Starting Instance: {0}...State: {1}".format(i.id,resp['StartingInstances'][0]['CurrentState']['Name']))
+    return
 
 if __name__ == '__main__':
     
-    list_ec2()
+    instances()
 
     
